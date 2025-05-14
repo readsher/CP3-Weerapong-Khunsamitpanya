773c43d3acb1b2c8d45edbfea02897d84c973628
readsher <readsher1234@gmail.com>
2025-05-14 13:47:59 +0700

    Exercise4
    ส่งงาน

---
+++ Exercise4_Weerapong_K.py
@@ -0,0 +1,11 @@
+SC1=80.0
+SC2=80.0
+SC3=80.0
+SC4=80.0
+
+
+print("--- Your Score ---")
+print("Foundation English : ",SC1)
+print("General Business :",SC2)
+print("Introduction to Computer Systems : ",SC3)
+print("Computer Programming :",SC4)
